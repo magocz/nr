5ca728085ef3da4e7b33b3c1f8e62543f6d3bd00
Neldam <msgocz@gmial.com>
2016-04-27 23:44:18 +0200

    New functionality

---
+++ service/rest/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mateusz
+ * Date: 27.04.2016
+ * Time: 22:55
+ */
---
+++ service/rest/season.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mateusz
+ * Date: 27.04.2016
+ * Time: 23:12
+ */
