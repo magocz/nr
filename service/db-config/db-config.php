32c143c6329acde2ed16ebb5ea6634b37865a6ac
Neldam <msgocz@gmial.com>
2016-05-08 21:44:11 +0200

    Dodanie pliku db-congif do gitignore

--- service/db-config/db-config.php
+++ service/db-config/db-config.php
@@ -8,7 +8,7 @@ $checkConnection = false;
 $isLoginRequired = false;
 
 // Create connection
-$dbcon = mysqli_connect("188.40.44.195:3306", "neldam_nr","Puma1234");
+$dbcon = mysqli_connect("188.40.44.195:3306", "neldam_nr","Admin1234");
 mysqli_select_db($dbcon, "neldam_nr");
 
 // Check connection
