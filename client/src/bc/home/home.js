1c43f31a187aef8e9678ddfdfd31f14119a84ea6
Mateusz Walter Gocz <mateusz.gocz@capgemini.com>
2016-05-09 20:05:22 +0200

    Dodanie nowej tabeli i wgl, kurwa syf

--- client/src/bc/home/home.js
+++ client/src/bc/home/home.js
@@ -4,8 +4,37 @@ $(function () {
     $("#activeSeasonCost").load("client/html/home/costToPlantsChar.html"); // load first Column chart
     $("#homeTableCell").load("client/html/home/home-table.html"); // load home table
 
-    // load data to the table
-    $('#table_id').DataTable();
+    $.ajax({
+        url: "service/rest/season.php/home-table/",
+        type: "GET",
+        dataType: 'json',
+        contentType: "application/x-www-form-urlencoded",
+        statusCode: {
+            200: function (homeData) {
+                drawTable(homeData.data);
+            }
+        }
+    });
+
+    function drawTable(data) {
+        for (var i = 0; i < data.length; i++) {
+            drawRow(data[i]);
+        }
+    }
+
+    function drawRow(rowData) {
+        var row = $("<tr />")
+        $("#homeTableActiveSeasonData").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+        row.append($("<td>" + rowData.fieldNumber + "</td>"));
+        row.append($("<td>" + rowData.description + "</td>"));
+        row.append($("<td>" + rowData.plant + "</td>"));
+        row.append($("<td>" + rowData.varietes + "</td>"));
+        row.append($("<td>" + rowData.ha + "</td>"));
+        row.append($("<td>" + rowData.operationsNumber + "</td>"));
+        row.append($("<td onclick='myfunction()'><i class='fa fa-arrow-circle-o-right' style='font-size:28px; cursor:pointer;'><a name='dupa'></a></i></td>"));
+
+    }
+
 
     $('#activeSeasonChartCombobox').change(function () {
         if ($('#activeSeasonChartCombobox').val() === 'plantsToVarietes') {
@@ -19,5 +48,9 @@ $(function () {
         }
     });
 
+
 });
 
+function myfunction() {
+    console.log('dsds');
+}
